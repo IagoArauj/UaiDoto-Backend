5d4e85ba26bcffbe5dcef1c818f1136efb78c1d5
Iago Araújo <iagoaoliveira14@outlook.com>
2022-04-20 11:01:08 -0300

    Initial setup
    Created a fresh project from spring initializer

---
+++ src/main/java/br/edu/ufsj/tp/TpApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ufsj.tp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpApplication.class, args);
+	}
+
+}
