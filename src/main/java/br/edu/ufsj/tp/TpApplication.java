912d47c9c96c304110f0aef547d51439b2de0376
Iago Araújo <iagoaoliveira14@outlook.com>
2022-06-08 15:15:27 -0300

    Change cors to accept all origins

--- src/main/java/br/edu/ufsj/tp/TpApplication.java
+++ src/main/java/br/edu/ufsj/tp/TpApplication.java
@@ -31,7 +31,7 @@ public class TpApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+				registry.addMapping("/**").allowedOrigins("*");
 			}
 		};
 	}
